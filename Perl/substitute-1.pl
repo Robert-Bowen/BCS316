e76c19705e49f4f18d7ab077f8126d1fa36cd2b1
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-04-26 19:29:07 -0400

    Add files via upload

---
+++ Perl/substitute-1.pl
@@ -0,0 +1,16 @@
+use 5.13.0;
+#Create original string
+my $originalString = "Yahoo is the most popular search engine on the Internet";
+#Copy the original string to the $newString variable
+my $newString = $originalString;
+#Perform the substitution of the word Yahoo for the word Google
+$newString =~ s/Yahoo/Google/; # In $file_name, remove any Unix-style path
+print "The original string was - $originalString \n";
+print "The new string is - $newString \n";
+
+
+
+#
+# You can also do it this way and save yourself from having to copy the variable.
+#(my $newOptional = $originalString) =~ s/Yahoo/Google/;
+#print "The other way of doing the substitution resulted in the string $newOptional\n";
