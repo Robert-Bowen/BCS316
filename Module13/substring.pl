5bf5773dda678a16a2f7f5585f647dea7ff32ec0
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-04-30 14:43:20 -0400

    Update substring.pl

--- Module13/substring.pl
+++ Module13/substring.pl
@@ -1,14 +1,12 @@
-#!/usr/bin/perl
-
 use strict;
 use warnings;
 use 5.13.0;
 use Term::ANSIScreen qw(cls);
-cls();
+cls(); #Clears the screen without having to call an OS process to do it.
 
 
 sub fixscreen {my $counter = 0; while ($counter < 5) {say "";$counter ++;}}
-fixscreen;
+fixscreen; #Outputs 5 blank lines
 
 say "Finds the word jumps which is at position 20 and is 5 characters long.";
 say "We found this by manually counting.  We can use index to help with this.";
