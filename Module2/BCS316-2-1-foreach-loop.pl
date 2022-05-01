c05d6a4bdf87a453838040adeaf4f9f0737e1f9d
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-01 15:24:07 -0400

    Update and rename foreach.pl to BCS316-2-1-foreach-loop.pl

--- Module2/BCS316-2-1-foreach-loop.pl
+++ Module2/BCS316-2-1-foreach-loop.pl
@@ -1,3 +1,6 @@
+#RB-BCS316
+#Example of Simple foreach loop.  Loops through 16 times (because it starts at zero.)
+
 use 5.28.0; 	#Note the use of the "\n" in the print statement
 			#since we aren't using say.
 #Sets up a loop from 0 to 15
