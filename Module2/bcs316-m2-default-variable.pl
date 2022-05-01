ed00deff1cbffe063970d5b9d5e633ed3746d86e
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-01 18:14:08 -0400

    Update and rename default-variable.pl to bcs316-m2-default-variable.pl

---
+++ Module2/bcs316-m2-default-variable.pl
@@ -0,0 +1,6 @@
+#RB-BCS316
+#Example of using the default variable.  We set the default variable to "Robert" and then output it.
+use 5.28.0;
+$_ = "Robert";
+print;
+print "\n";
--- Module2/default-variable.pl
+++
@@ -1,3 +0,0 @@
-$_ = "Robert";
-print;
-print "\n";
