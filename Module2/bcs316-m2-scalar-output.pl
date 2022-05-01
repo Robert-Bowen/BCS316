900bb035533ad57be132a10e06285a5bff51f4f6
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-01 18:13:14 -0400

    Update and rename hello-name.pl to bcs316-m2-scalar-output.pl

--- Module2/bcs316-m2-scalar-output.pl
+++ Module2/bcs316-m2-scalar-output.pl
@@ -1,4 +1,6 @@
-use 5.20.0; #Allows us to use the say function
+#RB-BCS316
+#Example of using scalar variables with output
+use 5.28.0; #Allows us to use the say function
 my $Fname; #Predefine the scalars $Fname and $Lname
 my $Lname;
 $Fname = 'Robert'; #Sets the value of $Fname and $Lname
