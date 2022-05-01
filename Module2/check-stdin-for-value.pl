0fa227a6cbbf8c320941164d6975ab707ac4e630
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-01 18:11:05 -0400

    Added header and description

--- Module2/check-stdin-for-value.pl
+++ Module2/check-stdin-for-value.pl
@@ -1,4 +1,8 @@
-use 5.20.0; #Allows us to use say
+#RB-BCS316
+#Example of taking input from the user (via STDIN) and then using the chomp function to remove the newline character
+#Then the input is checked and if it matches "Robert" we output "Hello Robert".  
+# or if it's not, we output "Hello not Robert"
+use 5.28.0; #Allows us to use say
 my $name;
 say "What is your name?";
 $name = <STDIN>;
