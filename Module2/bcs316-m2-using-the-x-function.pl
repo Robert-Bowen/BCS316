ae1669deedb8daa2f1ad905a93097bfc87253a05
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-01 18:20:28 -0400

    Update and rename using-the-x-function.pl to bcs316-m2-using-the-x-function.pl

---
+++ Module2/bcs316-m2-using-the-x-function.pl
@@ -0,0 +1,10 @@
+#RB-BCS316
+#Example of using the "x" to display multiple columns of the same data.
+# the script displays one line with 10 characters, the next with 20 characters and the last with 40 characters.
+$str= "x" x 10;
+use 5.28.0;
+print $str . "\n";
+$str1= "x" x 20;
+print $str1 . "\n";
+$str2= "x" x 40;
+print $str2 . "\n";
--- Module2/using-the-x-function.pl
+++
@@ -1,6 +0,0 @@
-$str= "x" x 10;
-print $str . "\n";
-$str1= "x" x 20;
-print $str1 . "\n";
-$str2= "x" x 40;
-print $str2 . "\n";
