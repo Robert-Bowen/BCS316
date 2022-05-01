ae2128107de8135171a9710fd86182932f2070a7
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-01 18:16:44 -0400

    Update and rename infinite-loop.pl to bcs316-m2-infinite-loop.pl

--- Module2/bcs316-m2-infinite-loop.pl
+++ Module2/bcs316-m2-infinite-loop.pl
@@ -4,4 +4,4 @@ use 5.28.0;
 say "We are about to start an infinite loop";
 say "Press enter to continue.  To end the loop press control+c or close the window";
 my $promptTheUser = <STDIN>; # we don't use this, it's just to get the user to press enter.
-for (;;) {print "It's an infinite loop!\n";}
+for (;;) {print "It's an infinite loop!\n";} # the for (;;) says until nothing happens check for nothing so it always evaluates to true.
