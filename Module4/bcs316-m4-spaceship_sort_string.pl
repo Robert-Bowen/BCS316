ea05a1dd2fb3456232b152ad798ed1cd7f6b6588
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-02 11:20:10 -0400

    Update bcs316-m4-spaceship_sort_string.pl

--- Module4/bcs316-m4-spaceship_sort_string.pl
+++ Module4/bcs316-m4-spaceship_sort_string.pl
@@ -1,5 +1,6 @@
 #RB-BCS316
 #Shows the spaceship sorting method.  We will go into further detail in a later module.
+# It is important to note that the spaceship sort only works on numbers.  This has letters in it to display this.
 use 5.23.0;
 say "";
 my @numb = qw(X a 12 r 14 A 6 65 Z 44 F 91 9 10);
