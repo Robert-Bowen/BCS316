649a90e91c9b35cdd1e66eb820736104190e6239
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-02 11:16:27 -0400

    Update and rename array-loop-w-scalar.pl to bcs316-m4-array-loop-w-scalar.pl

--- Module4/bcs316-m4-array-loop-w-scalar.pl
+++ Module4/bcs316-m4-array-loop-w-scalar.pl
@@ -1,12 +1,13 @@
+#RB-BCS316
 #Uses a foreach loop with a scalar variable to loop through
 # the array and show all of the elements.
 
 #This is the more common way of doing things.
 
-use 5.28.0;
+use 5.23.0;
 use strict;
 use warnings;
-my @Array = qw (Santa Easter_Bunny Leprechaun Yoda);
+my @Array = qw (Santa Easter_Bunny Leprechaun Yoda); #Creates array with 4 elements
 
 foreach my $element (@Array)
 {
