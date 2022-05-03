87fa545705bfee20addc720d69067d1074137e96
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 11:11:36 -0400

    Update and rename read.pl to bcs316-m8-readfile-1.pl

--- Module8/bcs316-m8-readfile-1.pl
+++ Module8/bcs316-m8-readfile-1.pl
@@ -1,3 +1,7 @@
+#RB-BCS316
+#This script shows an example of how to read data from a file and match it.  This matches all of the lines with Robert, (so we don't grab Roberta or Roberto).
+# feel free to substitute your own name and/or try other names.
+use 5.23.0;
 my $filename = "NY.txt";
 my $birthTotal = 0;
   open( my $fh, '<', $filename ) or die "Can't open $filename: $!";
