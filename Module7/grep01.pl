7c93bcad79ab4ab7af82398c09be7415b797ea20
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 12:12:51 -0400

    Update grep01.pl

--- Module7/grep01.pl
+++ Module7/grep01.pl
@@ -1,5 +1,7 @@
-#!/usr/bin/perl
-use 5.13.0;
+#RB-BCS316
+#This script shows an example of using a simple grep.  We will do more with regular expressions in Modules 9 and 10
+use 5.23.0;
+
 use Data::Dumper qw(Dumper); 
 my @days=qw(Monday Tuesday Thursday Friday);
 my @result=grep /Mon/, @days;
