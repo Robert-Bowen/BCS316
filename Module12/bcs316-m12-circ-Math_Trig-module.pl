c44277d4dfbfe6bc1485b3eca53df03766821ee7
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 10:15:07 -0400

    Update and rename circ_true_pi.pl to bcs316-m12-circ-Math_Trig-module.pl

--- Module12/bcs316-m12-circ-Math_Trig-module.pl
+++ Module12/bcs316-m12-circ-Math_Trig-module.pl
@@ -1,7 +1,6 @@
-#!/usr/bin/perl
-use 5.13.0;
-use strict;
-use warnings;
+#RB-BCS316
+#This script shows an example of how to determine the circumference of a circle using a true version of pi (more accurate version.)
+use 5.23.0;
 use Math::Trig;
 my $radius;
 my $pi = pi();
