380375824fd3a3c20ee107634d0425644203577f
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 10:32:34 -0400

    Update and rename printf.pl to bcs316-m13-printf.pl

--- Module13/bcs316-m13-printf.pl
+++ Module13/bcs316-m13-printf.pl
@@ -1,9 +1,6 @@
-use strict;
-use warnings;
-use 5.13.0;
-
-
+#RB-BCS316
+#This script shows an example of how to use the printf function.
+use 5.23.0;
 my @items = qw( Dwight Jim Michael Andy Phyllis Angela Darryl Oscar Kevin Creed Pam );
 my $format = "The items are:\n" . ("%10s\n" x @items); 
-## print "the format is >>$format<<\n"; # for debugging 
 printf $format, @items;
