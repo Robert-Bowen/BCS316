971f1eb73fd0c8f531dcfa0b7000e63c21ebf454
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 10:08:21 -0400

    Update and rename readkey.pl to bcs316-m12-readkey-module.pl

--- Module12/bcs316-m12-readkey-module.pl
+++ Module12/bcs316-m12-readkey-module.pl
@@ -1,3 +1,6 @@
+#RB-BCS316
+#This script shows an example of how to use the Term::Readkey module to "hide" input.  Term::Readkey has a number of useful functions.
+use 5.23.0;
 use Term::ReadKey;
 print "Please enter a password\n";
 print "The password will not display anything as you are typing it.\n";
