1d20ba7178c81c54df532bd156fe97e1737194c4
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 10:43:39 -0400

    Update and rename substring.pl to bcs316-m13-substring-1.pl

--- Module13/bcs316-m13-substring-1.pl
+++ Module13/bcs316-m13-substring-1.pl
@@ -1,18 +1,10 @@
-use strict;
-use warnings;
-use 5.13.0;
-use Term::ANSIScreen qw(cls);
-cls(); #Clears the screen without having to call an OS process to do it.
-
-
-sub fixscreen {my $counter = 0; while ($counter < 5) {say "";$counter ++;}}
-fixscreen; #Outputs 5 blank lines
+#RB-BCS316
+#This script shows an example of using the substr (substring) function.
+use 5.23.0;
 
 say "Finds the word jumps which is at position 20 and is 5 characters long.";
 say "We found this by manually counting.  We can use index to help with this.";
 
-fixscreen;
 my $phrase = "the quick brown fox jumps over the lazy dog.  why is that darn dog so utterly lazy?";
 my $action = substr ($phrase, 20, 5);
 say $action;
-fixscreen;
