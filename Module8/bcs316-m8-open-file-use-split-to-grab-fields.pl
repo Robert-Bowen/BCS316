e6a9abe0aa13a41d4c141dd93aaf601de6c5ad15
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 11:57:35 -0400

    Update and rename read-file2.pl to bcs316-m8-open-file-use-split-to-grab-fields.pl

--- Module8/bcs316-m8-open-file-use-split-to-grab-fields.pl
+++ Module8/bcs316-m8-open-file-use-split-to-grab-fields.pl
@@ -1,10 +1,12 @@
+#RB-BCS316
+#Opens a file and uses the split function on each line.  
+use 5.23.0;
 my @list;
-use 5.28.0;
 my $file = "vgsales.csv";
 open (my $csv, '<', $file) || die "cant open";
 foreach (<$csv>) {
    chomp;
    my @fields = split(/\,/);
-   push @list, $fields[1];
+   push @list, $fields[1]; #$fields[1] is the 2nd column.
    say $fields[1];
 }
