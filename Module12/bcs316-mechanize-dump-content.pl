aecda41317e9d40046090b56e8391ac52f5aa588
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 09:57:08 -0400

    Update and rename mechanize-content.pl to bcs316-mechanize-dump-content.pl

--- Module12/bcs316-mechanize-dump-content.pl
+++ Module12/bcs316-mechanize-dump-content.pl
@@ -1,6 +1,7 @@
+RB-BCS316
 #Uses WWW::Mechanize to dump all of the content it finds in the website stored in $url
 #This is cleaner than using the code from mechanize-crawl.pl
-use 5.18.0;
+use 5.23.0;
 use WWW::Mechanize;
     my $mech = WWW::Mechanize->new();
     my $url = "http://www.farmingdale.edu";
