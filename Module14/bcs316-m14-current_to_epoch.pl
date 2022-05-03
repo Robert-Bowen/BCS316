3380a4e1bb04474ca85c1e7f0247763d50839df9
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 10:56:39 -0400

    Update and rename current_to_epoch.pl to bcs316-m14-current_to_epoch.pl

--- Module14/bcs316-m14-current_to_epoch.pl
+++ Module14/bcs316-m14-current_to_epoch.pl
@@ -1,4 +1,7 @@
-use 5.13.0;
+#RB-BCS316
+#This script shows an example of reading the time from the OS and converting it to epoch (unix time).
+use 5.23.0;
+
 # $hours, $minutes, and $seconds represent a time today,
 # in the current time zone
 use Time::Local;
