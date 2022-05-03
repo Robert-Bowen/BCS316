901b9a64859f79636e1a12ecdd5ec44436727de8
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 12:13:50 -0400

    Update and rename grep_review.pl to bcs316-m7-using-grep-2.pl

---
+++ Module7/bcs316-m7-using-grep-2.pl
@@ -0,0 +1,7 @@
+#RB-BCS316
+#This script shows another example of using a simple grep.  We will do more with regular expressions in Modules 9 and 10
+use 5.23.0;
+use Data::Dumper qw(Dumper); 
+my @days=qw(Muno Plex Toodee Brobee Foofa );
+my @result=grep /oo/, @days;
+say Dumper \@result;
