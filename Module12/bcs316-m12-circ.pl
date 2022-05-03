12f3e9b0f4df451b99789af1f5e134a00e874b49
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 10:19:20 -0400

    Update bcs316-m12-circ.pl

--- Module12/bcs316-m12-circ.pl
+++ Module12/bcs316-m12-circ.pl
@@ -1,5 +1,6 @@
 #RB-BCS316
 #This script shows an example of how to determine the circumference of a circle.  bcs316-m12-circ-Math_Trig-module.pl shows how to do it using true pi.
+use 5.23.0;
 my $radius;
 say "What is the circumference of the circle? ";
 my $circumference = <STDIN>;
