065b7f3252738f91474fb2338f3da7f98d556892
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 09:37:08 -0400

    Update and rename join2.pl to bcs316-m10-join2.pl

--- Module10/bcs316-m10-join2.pl
+++ Module10/bcs316-m10-join2.pl
@@ -1,5 +1,7 @@
+#RB-BCS316
+#This example expands on how to use the join function.
+use 5.23.0;
 #User provides some values on one line and then a character to use as the "glue"
-use 5.13.0;
 say "Enter three values (words, numbers, mixed, etc.) and press return after each one";
 my $input1 = <STDIN>;
 chomp $input1;
@@ -9,7 +11,6 @@ chomp $input2;
 say "Value 3?";
 my $input3 = <STDIN>;
 chomp $input3;
-
 my $x = (join ":", $input1, $input2, $input3); 
 say "$input1, $input2, $input3 joined by colons are: $x";
 
