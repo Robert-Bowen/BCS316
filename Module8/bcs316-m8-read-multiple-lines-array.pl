bce8e98fd76c3eafbe34628c6baf0debb2c36444
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 11:58:54 -0400

    Update and rename read-multiple-lines-array.pl to bcs316-m8-read-multiple-lines-array.pl

--- Module8/bcs316-m8-read-multiple-lines-array.pl
+++ Module8/bcs316-m8-read-multiple-lines-array.pl
@@ -1,10 +1,8 @@
+#RB-BCS316
+#Opens up a file and reads it into an array.  Then displays the 1st and 5th lines (element 0 and 4)
+use 5.23.0;
 open(BIGFILE,"data1.txt") || die "Can't open: $!";
 @list=<BIGFILE>;
 close BIGFILE;
-$num = 1;
 print $list[0] . "\n";
 print $list [4] . "\n";
-
-
-
-
