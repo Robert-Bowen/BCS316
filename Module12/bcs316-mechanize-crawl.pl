bcba8538d48cc29c4bf024c798c1945f21fdbc92
Robert-Bowen <90286450+Robert-Bowen@users.noreply.github.com>
2022-05-03 09:57:30 -0400

    Update and rename mechanize-crawl.pl to bcs316-mechanize-crawl.pl

--- Module12/bcs316-mechanize-crawl.pl
+++ Module12/bcs316-mechanize-crawl.pl
@@ -1,7 +1,8 @@
+#RB-BCS316
 #This will get all of the content from the webpage defined in $URL and output it.
 #It's useful both to show how to use WWW::Mechanize and to be able to download
 #a site into a file
-use 5.18.0;
+use 5.23.0;
 use WWW::Mechanize;
 my $URL = 'http://www.mozilla.org';
 my $mech = WWW::Mechanize->new();
